07e4e9e55473094f513c6340d80926ea067d112b
Irfan Ahmed <irfaniahmed4@gmail.com>
2018-03-23 21:04:50 -0700

    Add files via upload

---
+++ app_client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Homepage/Homepage";
+import landing from "./components/Landing/Landing";
+import profile from "./components/Profile/Profile";
+import Nav from "./components/Nav/Nav.js";
+import NavTwo from "./components/Nav/NavTwo.js";
+import Title from "./components/Title/Title.js";
+import Header from "./components/Header/Header.js";
+import AddLeague from "./components/AddLeague/AddLeague.js";
+import Footer from "./components/Footer/Footer.js";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App Site">
+          <div>
+            <ProtectedRoute exact path="/" component={Home} />
+            <Route exact path="/Landing" component={landing} />
+            <Route exact path="/Home" component={Home} />
+          </div>
+        </div>
+      </Router>
+
+    )
+  }
+};
+
+export default App;
---
+++ app_client/src/components/Homepage/Homepage.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import NavTwo from "../Nav/NavTwo.js";
+import Title from "../Title/Title.js";
+import Footer from "../Footer/Footer.js";
+import "./Homepage.css"
+import MyForm from './forms.js';
+import {render} from 'react-dom';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App Site">
+                <div className="Site-content">
+
+                    <NavTwo />
+                    <Title />
+                    <div className="sign-in">
+                    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">
+  Join the Movement
+</button>
+<div className="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Register as New User</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+      <form>
+      <div class="form-group">
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username"/>
+  </div>
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  
+  <div className="form-check">
+  <label className="radio-inline"/>
+  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> Stay signed in
+
+  <label className="radio-inline"/>
+  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> Forgot Password
+  </div>
+    <div>
+  <button type="submit" class="btn btn-primary">Create Account</button>
+  </div>
+</form>
+      </div>
+    </div>
+  </div>
+</div>
+                        <br />
+                        <br />
+                        <div>
+                            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                SIGN IN!
+                        </button>
+
+                            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-dialog" role="document">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h5 className="modal-title" id="exampleModalLabel">SIGN INTO GREATNESS!</h5>
+                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div className="modal-body">
+                                            <MyForm />
+                                        </div>
+                                        <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home; 
